3f7d634b752ea97c77a8a8ebba8726d16f9a72d4
jhkim7997 <cogoma97@gmail.com>
2023-01-31 20:57:21 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+//App.js
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+//customer 객체를 생성 하여 prop값 전달
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '김두환',
+    birthday: '989797',
+    gender: '남자',
+    job: '건달'
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '이두봉',
+    birthday: '559797',
+    gender: '남자',
+    job: '군인'
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '이보희',
+    birthday: '779797',
+    gender: '여자',
+    job: '간호사'
+  }
+]
 function App() {
   return (
-    <div className="gray-background">
-     <img src={logo} lat="logo"/>
-     <h2>Let's develop management system!!</h2>
-    </div>
+    <div>
+      {customers.map(c => {  //map을 이용해 배열의 값을 순차적으로 가져온다. 
+        return <Customer
+          key={c.id} //idx인덱스를 할당 해줘야 리액트에서는 경고가 뜨지 않음.
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+        />;  
+      })}
+
+  </div>
   );
 }
 
