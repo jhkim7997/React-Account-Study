39bd555dddc6511bcbe00f0eeca51b42d62e50af
jhkim7997 <cogoma97@gmail.com>
2023-02-01 22:00:29 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+import { withStyles } from '@material-ui/core/styles'; // 스타일 라이버러리
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+
+  },
+  table:{
+    minWidth: 1080
+  }
+})
+
 //customer 객체를 생성 하여 prop값 전달
 const customers = [
   {
@@ -30,23 +52,37 @@ const customers = [
     job: '간호사'
   }
 ]
-function App() {
+function App(props) {
+  const {classes} = props;
   return (
-    <div>
-      {customers.map(c => {  //map을 이용해 배열의 값을 순차적으로 가져온다. 
-        return <Customer
-          key={c.id} //idx인덱스를 할당 해줘야 리액트에서는 경고가 뜨지 않음.
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-        />;  
-      })}
-
-  </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {  //map을 이용해 배열의 값을 순차적으로 가져온다. 
+            return <Customer
+              key={c.id} //idx인덱스를 할당 해줘야 리액트에서는 경고가 뜨지 않음.
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />;
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,37 +1,20 @@
 //Customer.js
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 const Customer = (customer) => {
 
     return (
-        <div>
-            <CustomerProfile id={customer.id} image={customer.image} name={customer.name}/>
-            <CustomerInfo birthday={customer.birthday} gender={customer.gender} job={customer.job}/>
-        </div>
-    );
-
-};
-
-const CustomerProfile = (customerProfile) => {
-
-    return (
-        <div>
-            <img src={customerProfile.image} alt="profile"/>
-            <h2>{customerProfile.name}({customerProfile.id})</h2>
-        </div>
-    );
-
-};
-
-const CustomerInfo = (customerInfo) => {
-
-    return (
-        <div>
-            <p>{customerInfo.birthday}</p>
-            <p>{customerInfo.gender}</p>
-            <p>{customerInfo.job}</p>
-
-        </div>
+        <TableRow>
+            <TableCell>{customer.id}</TableCell>
+            <TableCell><img src={customer.image} alt="profile"/></TableCell>
+            <TableCell>{customer.name}</TableCell>
+            <TableCell>{customer.birthday}</TableCell>
+            <TableCell>{customer.gender}</TableCell>
+            <TableCell>{customer.job}</TableCell>
+        </TableRow>
+        
     );
 
 };
