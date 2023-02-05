aa1d07c2c777b84c50d633762fbe520a8f8e2d18
jhkim7997 <cogoma97@gmail.com>
2023-02-05 20:08:00 +0900

    DB connect TEST

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 5000;
 
+//===================Mysql접속 ===================
+const fs = require('fs');
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host:conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+//===================Mysql접속 ===================
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.get('/api/hello', (req, res) => {
   res.send('Hello World!')
@@ -12,33 +31,14 @@ app.get('/api/hello', (req, res) => {
 //응답은 bodyParser.json()사용하여 알아서 json형태로 전달하게 만들었다.
 app.get('/api/customer', (req, res) => {
   //for(let i=0; i < 9999999999; i++){} //생각하는 시간
-
-  res.send([
-    {
-      id: 1,
-      image: 'https://placeimg.com/64/64/1',
-      name: '김갑환',
-      birthday: '989797',
-      gender: '남자',
-      job: '사범'
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/64/64/2',
-      name: '이정재',
-      birthday: '559797',
-      gender: '남자',
-      job: '배우'
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/64/64/3',
-      name: '김사라',
-      birthday: '779797',
-      gender: '여자',
-      job: '전도사'
+  // 쿼리로 데이터 접속하기
+  connection.query(
+    "select * from customer",
+    (err,rows,fields) =>{
+      res.send(rows);
     }
-  ]);
+  );
+
 });
 
 app.listen(port, () => {
