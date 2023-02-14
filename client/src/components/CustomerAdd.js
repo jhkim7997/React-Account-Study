3325deaee127fb1e650ae4edcd4e8d0964aa427f
jhkim7997 <cogoma97@gmail.com>
2023-02-14 23:03:30 +0900

    Modal Add

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -2,7 +2,21 @@
 import React , {useState} from 'react';
 import Axios from 'axios'; //post방식으로 데이터 전달을 위해
 
-function CustomerAdd({onChangelist}){
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles'; // 스타일 라이버러리
+
+const styles = theme => ({
+    hidden: {
+     display: 'none'
+    }
+  })
+
+function CustomerAdd(props){
 
     const [file,setFile] = useState(null); 
     const [userName,setUserName] = useState(""); 
@@ -10,13 +24,14 @@ function CustomerAdd({onChangelist}){
     const [gender,setGender] = useState(""); 
     const [job,setJob] = useState(""); 
     const [fileName,setFileName] = useState(""); 
+    const [open,setOpen] = useState(false);
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
         addCustomer()
         .then((response) => {
             console.log(response.data);
-            onChangelist(); //App.js의 부모 함수 호출
+            props.onChangelist(); //App.js의 부모 함수 호출
         })
         setFile(null);
         setUserName("");
@@ -24,7 +39,21 @@ function CustomerAdd({onChangelist}){
         setGender("");
         setJob("");
         setFileName("");
+        setOpen(false); //전송후 Modal창 닫기 속성 추가
+    }
 
+    const handleOpen = () => {
+        setOpen(true); // Modal창 열기 속성 추가
+    }
+
+    const handleClose = () => {
+        setFile(null);
+        setUserName("");
+        setBirthday("");
+        setGender("");
+        setJob("");
+        setFileName("");
+        setOpen(false);// Modal창 당기 속성 추가 및 useState변수 초기화
     }
 
     const handleFileChange = (e) => {
@@ -61,8 +90,35 @@ function CustomerAdd({onChangelist}){
         //axios post사용
         return Axios.post(url, formData,config);
     }
-
+    const { classes } = props;
     return (
+         /* open이 'true일때 보여짐' Dialog닫을때 handleClose함수가 실행된 */
+        <div>
+            <Button variant="contained" color="primary" onClick={handleOpen}>
+                고객 추가하기
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>고객추가</DialogTitle>
+                <DialogContent>
+                    <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file"  file={file} value={fileName} onChange={handleFileChange}/>
+                    <label htmlFor="raised-button-file">
+                        <Button variant="contained" color="primary" component="span" name="file">
+                            {fileName === "" ? "프로필이미지 선택" : fileName}
+                        </Button>
+                    </label>
+                    <br></br>
+                    <TextField label="이름" type="text" name="userName"  value={userName} onChange={handleUserName}/><br></br>
+                    <TextField label="생년월일" type="text" name="birthday"  value={birthday} onChange={handleBirthday}/><br></br>
+                    <TextField label="성별" type="text" name="gender"  value={gender} onChange={handleGender}/><br></br>
+                    <TextField label="직업" type="text" name="job"  value={job} onChange={handleJob}/><br></br>
+                </DialogContent>
+                <DialogActions>
+                <Button variant="contained" color="primary" onClick={handleFormSubmit}>추가</Button>
+                <Button variant="outlined" color="primary" onClick={handleClose}>닫기</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+        /*
         <form onSubmit={handleFormSubmit}>
             <h1>고객추가</h1>
             프로필 이미지: <input type="file" name="file" file={file} value={fileName} onChange={handleFileChange}/><br></br>
@@ -72,8 +128,9 @@ function CustomerAdd({onChangelist}){
             직업: <input type="text" name="job"  value={job} onChange={handleJob}/><br></br>
             <button type="submit">추가하기</button>
         </form>
+        */
     );
 
 };
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,19 +1,51 @@
 //CustomerDelete.js
-import React  from 'react';
+import React , {useState} from 'react';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
 
 function CustomerDelete({onChangelist,delId}){
+    const [open,setOpen] = useState(false); // 모달창 열기닫기 useState속성
     
+    const handleOpen = () => {
+        setOpen(true); // Modal창 열기 속성 추가
+    }
+
+    const handleClose = () => {
+        setOpen(false);// Modal창 당기 속성 추가 및 useState변수 초기화
+    }
+
     const deleteCustomer = (id) => {
         const url = '/api/customers/'+id;
         fetch(url,{
             method: 'DELETE'
         });
-        //console.log("delId:",delId);
+        //console.log("delId:",delId);    <Button variant="contained" color="secondary" onClick={(e) => {deleteCustomer(delId)}}>삭제하기</Button>
         onChangelist(); //App.js의 부모 함수 호출
     }
 
     return (
-            <button onClick={(e) => {deleteCustomer(delId)}}>삭제하기</button>
+        <div>
+            <Button variant="contained" color="secondary" onClick={handleOpen}>삭제</Button>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle onClose={handleClose}>
+                삭제 경고
+            </DialogTitle>
+            <DialogContent>
+                <Typography gutterBottom>
+                    선택한 공객 정보가 삭제됩니다.
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                 <Button variant="contained" color="primary" onClick={(e) => {deleteCustomer(delId)}}>삭제</Button>
+                 <Button variant="outlined" color="primary" onClick={handleClose}>닫기</Button>
+            </DialogActions>
+        </Dialog>
+        </div>
     );
 
 };
